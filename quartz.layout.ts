797e1bed2dbffe6a0cd376944abb319fb1756db7
Czino <eric@czino.de>
2026-07-28 14:50:42 +0200

    Change graph depth to 1

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,6 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
-import * as Component from "./quartz/components"
-import { defaultOptions } from "./quartz/components/Graph"
+import { PageLayout, SharedLayout } from "./quartz/cfg";
+import * as Component from "./quartz/components";
+import { defaultOptions } from "./quartz/components/Graph";
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -13,7 +13,7 @@ export const sharedPageComponents: SharedLayout = {
       Patreon: "https://patreon.com/designelements",
     },
   }),
-}
+};
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
@@ -35,19 +35,23 @@ export const defaultContentPageLayout: PageLayout = {
       ...defaultOptions,
       localGraph: {
         ...defaultOptions.localGraph,
-        depth: 2,
+        depth: 1,
         showTags: false,
-        hideWithTags: ['source']
+        hideWithTags: ["source"],
       },
     }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-}
+};
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -56,4 +60,4 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
-}
+};
